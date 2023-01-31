af5f2613f93e17e715650535552fd8cdce3efdf3
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2023-01-31 22:28:22 +0530

    Added: Ability to find word using word

---
+++ src/com/bridgelabz/datastructure/algorithm/FindWord.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.datastructure.algorithm;
+
+import java.util.Scanner;
+
+public class FindWord {
+    public static void main(String[] args) {
+        String findWord[] = {"Ramesh", "Rajesh", "Mohit", "Rohit"};
+
+        System.out.println("Enter the word you want to find ");
+        String search = new Scanner(System.in).nextLine();
+        boolean flag = false;
+
+        for (String found : findWord) {
+            if (found.equals(search)) {
+                System.out.println(found + " has been found");
+                flag = true;
+                break;
+            }
+        }
+        if(flag==false)
+            System.out.println(search +" not found");
+    }
+}
